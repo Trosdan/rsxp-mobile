4af9ffb4f06e5462dbdb6f72f99dfac7c9523923
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2019-11-26 14:09:42 -0300

    Roteamento

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Home from './pages/Home';
+
+export default createAppContainer(createSwitchNavigator({
+    Home
+}))
