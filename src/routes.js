42af8973a5884bc6ac7dedfd0e702229b982ba57
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2019-11-27 11:25:17 -0300

    Correções

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,7 @@ import Challenge3 from './pages/Challenge3';
 import Challenge4 from './pages/Challenge4';
 import Challenge5 from './pages/Challenge5';
 
-<<<<<<< HEAD
-const Routes = createAppContainer(
+const DashBoard = createAppContainer(
   createStackNavigator(
     {
       Home,
@@ -29,29 +28,10 @@ const Routes = createAppContainer(
           backgroundColor: '#ffa500',
         },
         headerTintColor: '#FFF',
-=======
-const DashBoard = createStackNavigator(
-  {
-    Home,
-    Challenge1,
-    Challenge2,
-    Challenge3,
-    Challenge4,
-    Challenge5,
-  },
-  {
-    headerLayoutPreset: 'center',
-    headerBackTitleVisible: false,
-    defaultNavigationOptions: {
-      headerTitle: 'Desafios',
-
-      headerStyle: {
-        backgroundColor: '#ffa500',
->>>>>>> b29517d66d404335da8f0bb23a786734349f540b
       },
       headerTintColor: '#FFF',
     },
-  },
+  ),
 );
 
 const Routes = (isLogged = false) =>
